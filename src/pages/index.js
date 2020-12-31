f8675d9f1967de5f33b2976ffe313859d92a6b71
unknown <lily.li0597@gmail.com>
2020-12-31 18:30:35 -0500

    griffin moving his eyes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Home from '../components/home'
+import Home from "../components/home"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Home />
+    <Home />    
   </Layout>
 )
 
