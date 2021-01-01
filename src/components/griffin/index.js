27969b47597d94d665571b549281b63abe8777b9
Danny Li <dongledan@yahoo.com>
2020-12-31 22:45:25 -0500

    paths

--- src/components/griffin/index.js
+++ src/components/griffin/index.js
@@ -1,34 +1,36 @@
-import React from "react"
+import React, {useEffect} from "react"
 
 const Griffin = () => {
-  const rEye = document.querySelector(".r-eye")
-  const rPupil = document.querySelector(".r-pupil")
-  const lPupil = document.querySelector(".l-pupil")
-
-  if (rEye) {
-    const px = rPupil.getBoundingClientRect().width
-    const py = rPupil.getBoundingClientRect().height
-    const ex = rEye.getBoundingClientRect().width
-    const ey = rEye.getBoundingClientRect().height
-
-    const xMovement = (ex - px) / 2
-    const yMovement = (ey - py) / 2
-
-    const followMouse = e => {
-      const mouseX = e.clientX / document.body.clientWidth
-      const mouseY = e.clientY / document.body.clientHeight
-      const x = mouseX * 2 * xMovement
-      const y = mouseY * 2 * yMovement
-
-      rPupil.style.transform =
-        "translateX(" + x + "px)" + "translateY(" + y + "px)"
-      lPupil.style.transform =
-        "translateX(" + x + "px)" + "translateY(" + y + "px)"
+  useEffect(() => {
+    const rEye = document.querySelector(".r-eye")
+    const rPupil = document.querySelector(".r-pupil")
+    const lPupil = document.querySelector(".l-pupil")
+  
+    if (rEye) {
+      const px = rPupil.getBoundingClientRect().width
+      const py = rPupil.getBoundingClientRect().height
+      const ex = rEye.getBoundingClientRect().width
+      const ey = rEye.getBoundingClientRect().height
+  
+      const xMovement = (ex - px) / 2
+      const yMovement = (ey - py) / 2
+  
+      const followMouse = e => {
+        const mouseX = e.clientX / document.body.clientWidth
+        const mouseY = e.clientY / document.body.clientHeight
+        const x = mouseX * 2 * xMovement
+        const y = mouseY * 2 * yMovement
+  
+        rPupil.style.transform =
+          "translateX(" + x + "px)" + "translateY(" + y + "px)"
+        lPupil.style.transform =
+          "translateX(" + x + "px)" + "translateY(" + y + "px)"
+      }
+  
+      document.addEventListener("mousemove", followMouse, false)
     }
-
-    document.addEventListener("mousemove", followMouse, false)
-  }
-
+  })
+ 
   return (
     <div
       className="griffin"
